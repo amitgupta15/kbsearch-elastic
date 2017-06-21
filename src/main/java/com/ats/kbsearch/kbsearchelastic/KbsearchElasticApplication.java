142a1e2d15b44e7e1ae9285647ddd5f83552224a
Amit Gupta <amitgupta15@gmail.com>
2017-06-21 11:41:34 -0700

    Initial Commit

---
+++ src/main/java/com/ats/kbsearch/kbsearchelastic/KbsearchElasticApplication.java
@@ -0,0 +1,12 @@
+package com.ats.kbsearch.kbsearchelastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbsearchElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbsearchElasticApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ats/kbsearch/kbsearchelastic/domains/Book.java
@@ -0,0 +1,54 @@
+package com.ats.kbsearch.kbsearchelastic.domains;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+/**
+ * Created by amit on 6/13/17.
+ */
+
+@Document(indexName = "library", type = "books")
+public class Book {
+
+    @Id
+    private String id;
+    private String title;
+    private String author;
+    private String releaseDate;
+
+    public Book() {
+    }
+
+    public Book(String id, String title, String author, String releaseDate) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                '}';
+    }
+}
